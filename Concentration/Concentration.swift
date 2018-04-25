f4bbe8eddf532b1354ca4db0fbe0dd77e25068b9
kanestapler <kanestapler@gmail.com>
2018-04-24 22:45:28 -0400

    Lesson 1

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,15 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Kane Stapler on 4/24/18.
+//  Copyright © 2018 Kane Stapler. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    
+    
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let emojies = ["👻", "🎃", "👻", "🎃"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cards: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(emoji: emojies[cards.index(of: sender)!], button: sender)
     }
-
+    
+    func flipCard(emoji: String, button: UIButton) {
+        if (button.currentTitle == nil) {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            flipCount += 1
+        } else {
+            button.setTitle(nil, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+    }
 
 }
 
